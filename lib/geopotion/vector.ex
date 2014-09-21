e875ae5c65a774e0893ab3769e72a577b57f2206
Rodney Norris <Rodney.Norris@gmail.com>
2014-09-21 12:06:39 -0500

    initial impl of vector functions

--- lib/geopotion/vector.ex
+++ lib/geopotion/vector.ex
@@ -10,17 +10,23 @@ defmodule GeoPotion.Vector do
   @epsg_wgs1984 7030
   @equitorial_radius_wgs1984 6378137
   @polar_radius_wgs1984 6356752.3142
+  @flattening_wgs1984 0.0033528106718309896
   @inverse_flattening_wgs1984 298.257223563
-  
-  @calc_iterations 20
 
-  def distanceOverGround(%Position{} = from, %Position{} = to) do
-    _distanceOverGround(from.latitude, from.longitude, to.latitude, to.longitude)
-    |> Distance.new :km
+  @calc_iterations 100
+
+  def calculate(%{latitude: _,longitude: _} = from, %{latitude: _, longitude: _} = to) do
+    _vincentyCalc(from.latitude, from.longitude, to.latitude, to.longitude)
   end
 
-  def bearingTo(%Position{} = from, %Position{} = to) do
-    _bearingTo(from.latitude, from.longitude, to.latitude, to.longitude)
+  def distanceOverGround(%{latitude: _,longitude: _} = from, %{latitude: _, longitude: _} = to) do
+    %{distance: result} = _vincentyCalc(from.latitude, from.longitude, to.latitude, to.longitude)
+    result
+  end
+
+  def bearingTo(%{latitude: _, longitude: _} = from, %{latitude: _,longitude: _} = to) do
+    %{azimuth: result} = _vincentyCalc(from.latitude, from.longitude, to.latitude, to.longitude)
+    result
   end
 
   defp _distanceOverGround(lat1, lon1, lat2, lon2) do
@@ -39,10 +45,8 @@ defmodule GeoPotion.Vector do
     dLon = abs(rLon2 - rLon1)
 
     l = (rLat1 + rLat2) * 0.5
-    aKm = @equitorial_radius_wgs1984 |> Distance.new |> Distance.to_km
-    bKm = @polar_radius_wgs1984 |> Distance.new |> Distance.to_km
-    a = aKm.value
-    b = bKm.value
+    a = @equitorial_radius_wgs1984
+    b = @polar_radius_wgs1984
     e = Math.sqrt(1 - (b* b)  / (a * a))
     r1 = (a * (1 - (e * e))) / Math.pow((1 - (e * e) * (Math.sin(l) * Math.sin(l))), 3 * 0.5)
     r2 = a / Math.sqrt(1 - (e * e) * (Math.sin(l) * Math.sin(l)))
@@ -55,19 +59,20 @@ defmodule GeoPotion.Vector do
     ravg * c
   end
 
-  defp _bearingTo(lat1, lon1, lat2, lon2) when lat1 == lat2 and lon1 == lon2 do
-    Angle.new(0.0)
+  defp _vincentyCalc(lat1, lon1, lat2, lon2) when lat1 == lat2 and lon1 == lon2 do
+    %{azimuth: Angle.new(0.0), distance: Distance.new(0.0) }
   end
 
-  
-  defp _bearingTo(lat1, lon1, lat2, lon2) do
+
+  defp _vincentyCalc(lat1, lon1, lat2, lon2) do
     rLat1 = Angle.degrees_to_radians(lat1)
     rLat2 = Angle.degrees_to_radians(lat2)
     rLon1 = Angle.degrees_to_radians(lon1)
     rLon2 = Angle.degrees_to_radians(lon2)
 
-    #rLonDiff = abs(rLon2 - rLon1)
-    f = (@equitorial_radius_wgs1984 - @polar_radius_wgs1984) / @equitorial_radius_wgs1984
+    a = @equitorial_radius_wgs1984
+    b = @polar_radius_wgs1984
+    f = @flattening_wgs1984
     u1 = Math.atan((1-f) * Math.tan(rLat1))
     u2 = Math.atan((1-f) * Math.tan(rLat2))
 
@@ -80,16 +85,28 @@ defmodule GeoPotion.Vector do
       l = 2 * Math.pi - l
     end
 
-    {_,_,_,_,lamda,_alpha} = _calc(0,l, {u1, u2, f, l, l, 0})
-    
-    goodLamda = abs(lamda)
+    [lamda, alpha, sigma, cos2SigmaM] = _ittCalc(0,l, {u1, u2, f, l}, [l])
 
-    if(Math.sin(rLon2 - rLon1) * Math.sin(goodLamda) < 0) do
-      goodLamda = -goodLamda
+    lamda = abs(lamda)
+
+    if(Math.sin(rLon2 - rLon1) * Math.sin(lamda) < 0) do
+      lamda = -lamda
     end
 
-    numer = Math.cos(u2) * Math.sin(goodLamda)
-    denom = Math.cos(u1) * Math.sin(u2) - Math.sin(u1) * Math.cos(u2) * Math.cos(goodLamda)
+    #calculations for the distance
+    distU2 = (alpha |> Math.cos |> Math.pow 2) * ((Math.pow(a,2) - Math.pow(b,2)) / Math.pow(b,2))
+    aa = 1 + distU2 / 16384 * (4096 + distU2 * (-768 + u2 *(320 - 175 * distU2)))
+    bb = distU2 / 1024 * (256 + distU2 * (-128 + distU2 * (74 - 47 * distU2)))
+    deltaSigma = bb * Math.sin(sigma) * (cos2SigmaM + bb / 4 * (Math.cos(sigma) * (-1 + 2 *
+      Math.pow(cos2SigmaM, 2)) - bb / 6 * cos2SigmaM * (-3 + 4 * (sigma |> Math.sin |> Math.pow 2)) *
+      (-3 + 4 * Math.pow(cos2SigmaM,2))))
+
+    s = b * aa * (sigma - deltaSigma)
+    dist = s |> Distance.new
+
+    #calculations for forward azimuth
+    numer = Math.cos(u2) * Math.sin(lamda)
+    denom = Math.cos(u1) * Math.sin(u2) - Math.sin(u1) * Math.cos(u2) * Math.cos(lamda)
 
     a12 = Math.atan2(numer, denom)
 
@@ -97,47 +114,43 @@ defmodule GeoPotion.Vector do
       a12 = a12 + (2* Math.pi)
     end
 
-    a12 |> Angle.radians_to_degrees |> Angle.new
+    fwdAz = a12 |> Angle.radians_to_degrees |> Angle.new
+
+
+    %{azimuth: fwdAz, distance: dist}
   end
 
-  defp _calc(count, lamdaDif, vals) when count == 0 or (lamdaDif > 0.1e-6 and count <= @calc_iterations) do
-    {u1, u2, f, l, lamda, _} = vals
-    oldlamda = lamda
+  defp _ittCalc(count, lamdaDif,state, vals) when count == 0 or (lamdaDif > 0.1e-6 and count <= @calc_iterations) do
+    {u1, u2, f, l} = state
+    [lamda| _rest] = vals
 
-    sinsigma = _sinSigma(vals)
-    cossigma = _cosSigma(vals)
+    a = Math.pow((Math.cos(u2) * Math.sin(lamda)), 2)
+    b = Math.pow((Math.cos(u1) * Math.sin(u2) - Math.sin(u1) * Math.cos(u2) * Math.cos(lamda)),2)
+    sinsigma = Math.sqrt(a + b)
+
+    cossigma = Math.sin(u1) * Math.sin(u2) + Math.cos(u1) * Math.cos(u2) * Math.cos(lamda)
+
     sigma = Math.atan2(sinsigma,cossigma)
     alpha = Math.cos(u1) * Math.cos(u2) * Math.sin(lamda) / Math.sin(sigma)
       |> Math.asin
     cos2SigmaM = Math.cos(sigma) - 2.0 * Math.sin(u1) * Math.sin(u2) / Math.pow(Math.cos(alpha), 2)
-    
-    c = f / 16 * Math.pow(Math.cos(alpha),2) * (4 + f * (4 - 3 * Math.pow(Math.cos(alpha),2))) 
- 
-    lamda = l + (1 - c) * f * Math.sin(alpha) *
+
+    c = f / 16 * Math.pow(Math.cos(alpha),2) * (4 + f * (4 - 3 * Math.pow(Math.cos(alpha),2)))
+
+    newlamda = l + (1 - c) * f * Math.sin(alpha) *
             (sigma + c * Math.sin(sigma) *
             (cos2SigmaM + c * Math.cos(sigma) *
             (-1 + 2 * Math.pow(cos2SigmaM, 2))))
-    if(lamda > Math.pi) do
-      oldlamda = Math.pi
+    if(newlamda > Math.pi) do
+      newlamda = Math.pi
       lamda = Math.pi
     end
-    
-    lamdaDiff = abs(lamda - oldlamda)
 
-    _calc(count+1,lamdaDiff,{u1,u2,f,l,lamda, alpha})  
+    lamdaDiff = abs(newlamda - lamda)
 
-  end
-  defp _calc(_count, _lamda, vals), do: vals
-  defp _sinSigma(vals) do
-    {u1, u2, _f, _l, lamda, _} = vals
-    a = Math.pow((Math.cos(u2) * Math.sin(lamda)), 2)
-    b = Math.pow((Math.cos(u1) * Math.sin(u2) - Math.sin(u1) * Math.cos(u2) * Math.cos(lamda)),2)
-    Math.sqrt(a + b)
+    _ittCalc(count+1,lamdaDiff,state,[newlamda, alpha, sigma, cos2SigmaM])
   end
 
-  defp _cosSigma(vals) do
-    {u1, u2,_f, _l, lamda, _} = vals
-    Math.sin(u1) * Math.sin(u2) + Math.cos(u1) * Math.cos(u2) * Math.cos(lamda)
-  end
+  defp _ittCalc(_count, _lamda, state, vals), do: vals
 
 end
--- test/vector_test.exs
+++ test/vector_test.exs
@@ -12,13 +12,27 @@ defmodule VectorTest do
     #Asheville - 35.594736, -82.554802
     #DoG =  387.8207 km
 
-    nashville = getNashvillePos() 
+    nashville = getNashvillePos()
     asheville = getAshevillePos()
-    dist1 = Vector.distanceOverGround(nashville,asheville)
-    dist2 = Vector.distanceOverGround(asheville,nashville)
+    dist1 = Vector.distanceOverGround(nashville,asheville) |> GeoPotion.Distance.to_km
+    dist2 = Vector.distanceOverGround(asheville,nashville) |> GeoPotion.Distance.to_km
 
-    true = assert_in_delta(dist1.value, 387.8207, 0.001)
+    true = assert_in_delta(dist1.value, 387.9754, 0.0001)
     true = assert_in_delta(dist1.value, dist2.value, 0.00001)
+  end
+
+  test "calculate - nash to ashe" do
+    #Nashville - 36.151366, -86.796530
+    #Asheville - 35.594736, -82.554802
+    #DoG =  387.8207 km
+
+    nashville = getNashvillePos()
+    asheville = getAshevillePos()
+    vector = Vector.calculate(nashville,asheville)
+    #dist2 = Vector.calculate(asheville,nashville)
+
+    true = assert_in_delta(vector.distance.value, 387_975.4, 0.1)
+    true = assert_in_delta(vector.azimuth.value, 97.911731, 0.00001)
   end
 
   test "bearing to - nash to ___" do
@@ -29,7 +43,7 @@ defmodule VectorTest do
 
     bearing = Vector.bearingTo(nash1, nash2)
 
-    true = assert_in_delta(bearing.value, 53.571197,0.0001)
+    true = assert_in_delta(bearing.value, 53.571197,0.01e8)
   end
 
   #TODO  Add tests for all 4 quadrants, corssing anti meridian etc.
