4d33427884f1c4d550a07ce416a969fb418278db
Rodney Norris <Rodney.Norris@gmail.com>
2014-10-17 22:00:26 -0500

    unit test clean-up - seperation

--- test/angle_test.exs
+++ test/angle_test.exs
@@ -5,64 +5,48 @@ defmodule AngleTest do
 
   @conv_delta 0.0000001
 
-  test "angle - create new degrees" do
-    #test creation on Angle in degrees
-    assert Angle.new(310) == %Angle{value: 310 , units: :degrees}
-    assert Angle.new(0) == %Angle{value: 0, units: :degrees}
-    assert Angle.new(359.9) == %Angle{value: 359.9, units: :degrees}
-    assert Angle.new(-254) == %Angle{value: -254, units: :degrees}
+  test "angle - create new - 0", do: assert Angle.new(0) == %Angle{value: 0, units: :degrees}
+  test "angle - create new", do: assert Angle.new(310) == %Angle{value: 310 , units: :degrees}
+  test "angle - create new - 359.9", do: assert Angle.new(359.9) == %Angle{value: 359.9, units: :degrees}
+  test "angle - create new - negative", do: assert Angle.new(-254) == %Angle{value: -254, units: :degrees}
 
-  end
-
-  test "angle - degrees normalized" do
-    #test degrees are normailized correctly on creation 
-    assert Angle.new(370) |> Angle.normalize == %Angle{value: 10, units: :degrees}
-    assert Angle.new(-10) |> Angle.normalize == %Angle{value: 350, units: :degrees}
-    assert Angle.new(360) |> Angle.normalize == %Angle{value: 0, units: :degrees}
-  end
+  #test degrees are normailized correctly on creation 
+  test "angle - degrees normalized - 370", do: assert Angle.new(370) |> Angle.normalize == %Angle{value: 10, units: :degrees}
+  test "angle - degrees normalized - -10", do: assert Angle.new(-10) |> Angle.normalize == %Angle{value: 350, units: :degrees}
+  test "angle - degrees normalized - 360", do: assert Angle.new(360) |> Angle.normalize == %Angle{value: 0, units: :degrees}
   
-  test "angle - new degrees with atom" do
-    assert Angle.new(310, :degrees) == %Angle{value: 310 , units: :degrees}
-    assert Angle.new(0, :degrees) == %Angle{value: 0, units: :degrees}
-    assert Angle.new(359.9, :degrees) == %Angle{value: 359.9, units: :degrees}
-    #test degrees are normailized correctly on creation 
-    assert Angle.new(370, :degrees) |> Angle.normalize  == %Angle{value: 10, units: :degrees}
-    assert Angle.new(-10, :degrees) |> Angle.normalize  == %Angle{value: 350, units: :degrees}
-    assert Angle.new(360, :degrees) |> Angle.normalize  == %Angle{value: 0, units: :degrees}
-  end
+  test "angle - new degrees with atom - 310", do: assert Angle.new(310, :degrees) == %Angle{value: 310 , units: :degrees}
+  test "angle - new degrees with atom - 0", do: assert Angle.new(0, :degrees) == %Angle{value: 0, units: :degrees}
+  test "angle - new degrees with atom - float", do: assert Angle.new(359.9, :degrees) == %Angle{value: 359.9, units: :degrees}
+  #test degrees are normailized correctly on creation 
+  test "angle - new degrees with atom - 390", do: assert Angle.new(390, :degrees) |> Angle.normalize  == %Angle{value: 30, units: :degrees}
+  test "angle - new degrees with atom - -20", do: assert Angle.new(-20, :degrees) |> Angle.normalize  == %Angle{value: 340, units: :degrees}
+  test "angle - new degrees with atom - 360", do: assert Angle.new(360, :degrees) |> Angle.normalize  == %Angle{value: 0, units: :degrees}
 
-  test "angle - new radians" do
-    #test radians
-    assert Angle.new(0.0, :radians) == %Angle{value: 0.0, units: :radians}
-    assert Angle.new(:math.pi(), :radians) == %Angle{value: :math.pi(), units: :radians}
-  end
+  #test radians
+  test "angle - new radians", do: assert Angle.new(0.0, :radians) == %Angle{value: 0.0, units: :radians}
+  test "angle - new radians", do: assert Angle.new(:math.pi(), :radians) == %Angle{value: :math.pi(), units: :radians}
 
-  test "angle - new radians normalized" do
-    #test radians are normalized properly on creation 
-    assert Angle.new(-:math.pi(), :radians) |> Angle.normalize  == %Angle{value: :math.pi(), units: :radians}
-    assert Angle.new(:math.pi()*2, :radians) |> Angle.normalize  == %Angle{value: 0.0, units: :radians}
-    assert Angle.new(:math.pi()*3, :radians) |> Angle.normalize  == %Angle{value: :math.pi(), units: :radians}
-  end
-  
-  test "angle - normalize" do
-    assert Angle.new(370) |> Angle.normalize == %Angle{value: 10, units: :degrees}
-  end
+  #test radians are normalized properly on creation 
+  test "angle - new radians normalized - -pi", do: assert Angle.new(-:math.pi(), :radians) |> Angle.normalize  == %Angle{value: :math.pi(), units: :radians}
+  test "angle - new radians normalized - 2 pi", do: assert Angle.new(:math.pi()*2, :radians) |> Angle.normalize  == %Angle{value: 0.0, units: :radians}
+  test "angle - new radians normalized - 3 pi", do: assert Angle.new(:math.pi()*3, :radians) |> Angle.normalize  == %Angle{value: :math.pi(), units: :radians}
 
-  test "angle - is normalized" do
-    assert Angle.is_normalized(Angle.new(0.0)) == true    
-    assert Angle.is_normalized(Angle.new(180.0)) == true    
-    assert Angle.is_normalized(Angle.new(0.0, :radians)) == true    
-    assert Angle.is_normalized(Angle.new(:math.pi, :radians)) == true    
-    assert Angle.is_normalized(Angle.new(-:math.pi, :radians)) == false    
-    assert Angle.is_normalized(Angle.new(455.0)) == false    
-    assert Angle.is_normalized(Angle.new(-10.0)) == false    
-    assert Angle.is_normalized(Angle.new(4* :math.pi, :radians)) == false   
-  end
+  test "angle - is normalized - 0.0 degrees", do: assert Angle.is_normalized(Angle.new(0.0)) == true    
+  test "angle - is normalized - 180.0 degrees", do: assert Angle.is_normalized(Angle.new(180.0)) == true    
+  test "angle - is normalized - 0.0 radians", do: assert Angle.is_normalized(Angle.new(0.0, :radians)) == true    
+  test "angle - is normalized - pi, radians", do: assert Angle.is_normalized(Angle.new(:math.pi, :radians)) == true    
+  test "angle - is normalized - -pi, radians", do: assert Angle.is_normalized(Angle.new(-:math.pi, :radians)) == false    
+  test "angle - is normalized - 455.0 degrees", do: assert Angle.is_normalized(Angle.new(455.0)) == false    
+  test "angle - is normalized - -10.0 degrees", do: assert Angle.is_normalized(Angle.new(-10.0)) == false    
+  test "angle - is normalized - 4pi, radians", do: assert Angle.is_normalized(Angle.new(4* :math.pi, :radians)) == false   
 
   test "angle - degrees to radians" do
    conversion = Angle.degrees_to_radians(180.0) 
    true = assert_in_delta(conversion,:math.pi(),@conv_delta)
+  end
 
+  test "angle  - to_radians - 180.0" do
    convAngle = Angle.to_radians(Angle.new(180.0))
    assert convAngle.units == :radians
    true = assert_in_delta(convAngle.value, :math.pi(), @conv_delta)
@@ -71,19 +55,21 @@ defmodule AngleTest do
   test "angle - radians to degrees" do
     conversion = Angle.radians_to_degrees(:math.pi())
     true = assert_in_delta(conversion,180.0,@conv_delta) 
+  end
 
+  test "angle - to_degrees - pi" do
     convAngle = Angle.to_degrees(Angle.new(:math.pi(), :radians))
     assert convAngle.units == :degrees
     true = assert_in_delta(convAngle.value, 180.0, @conv_delta)
   end
 
-  test "angle - to degrees" do
+  test "angle - to degrees - no conversion needed" do
     original = Angle.new(270.0)
     conv = Angle.to_degrees(original)
     assert conv == original
   end
 
-  test "angle - to radians" do
+  test "angle - to radians - no conversion needed" do
     original = Angle.new(:math.pi() * 1.5, :radians)
     conv = Angle.to_radians(original)
     assert conv == original
--- test/distance_test.exs
+++ test/distance_test.exs
@@ -5,87 +5,80 @@ defmodule DistanceTest do
   
   @conv_delta 0.0001
 
-  test "dist - new" do
-    assert Distance.new(50) == %Distance{value: 50, units: :m}
-    assert Distance.new(50, :m) == %Distance{value: 50, units: :m}
-    assert Distance.new(100, :km) == %Distance{value: 100, units: :km}
-    assert Distance.new(345, :ft) == %Distance{value: 345, units: :ft}
-    assert Distance.new(50, :sm) == %Distance{value: 50, units: :sm}
-    assert Distance.new(5, :nm) == %Distance{value: 5, units: :nm}
-  end  
+  test "dist - new - 50 (auto meters)", do: assert Distance.new(50) == %Distance{value: 50, units: :m}
+  test "dist - new - 50, meters", do: assert Distance.new(50, :m) == %Distance{value: 50, units: :m}
+  test "dist - new - 100, km", do: assert Distance.new(100, :km) == %Distance{value: 100, units: :km}
+  test "dist - new - 345, feet", do: assert Distance.new(345, :ft) == %Distance{value: 345, units: :ft}
+  test "dist - new - 50, statute miles", do: assert Distance.new(50, :sm) == %Distance{value: 50, units: :sm}
+  test "dist - new - 5, nautical miles", do: assert Distance.new(5, :nm) == %Distance{value: 5, units: :nm}
 
-  test "dist - to meters" do
-    assert Distance.to_meters(%Distance{value: 100, units: :m}) == %Distance{value: 100, units: :m}
-    assert Distance.to_meters(%Distance{value: 1, units: :km}) == %Distance{value: 1000, units: :m}
-    
-    # decimal conversion
-    assert Distance.to_meters(%Distance{value: 100, units: :ft}) == %Distance{value: 30.48, units: :m}   
-    assert Distance.to_meters(%Distance{value: 1, units: :sm}) == %Distance{value: 1609.344, units: :m}   
-    assert Distance.to_meters(%Distance{value: 1, units: :nm}) == %Distance{value: 1852, units: :m}   
-  end
+  test "dist - to meters - 100m", do: assert Distance.to_meters(%Distance{value: 100, units: :m}) == %Distance{value: 100, units: :m}
+  test "dist - to meters - 1km", do: assert Distance.to_meters(%Distance{value: 1, units: :km}) == %Distance{value: 1000, units: :m}
+  test "dist - to meters - 100ft", do: assert Distance.to_meters(%Distance{value: 100, units: :ft}) == %Distance{value: 30.48, units: :m}   
+  test "dist - to meters - 1sm", do: assert Distance.to_meters(%Distance{value: 1, units: :sm}) == %Distance{value: 1609.344, units: :m}   
+  test "dist - to meters - 1nm", do: assert Distance.to_meters(%Distance{value: 1, units: :nm}) == %Distance{value: 1852, units: :m}   
 
-  test "dist - to kilometers" do
-    assert Distance.to_km(%Distance{value: 1.0, units: :km}) == %Distance{value: 1.0, units: :km}
-    assert Distance.to_km(%Distance{value: 1000, units: :m}) == %Distance{value: 1.0, units: :km}
-    assert Distance.to_km(%Distance{value: 1000, units: :nm}) == %Distance{value: 1852.0, units: :km}
-    
-    #decimal conversion
-    #feet to km
+  test "dist - to kilometers - km", do: assert Distance.to_km(%Distance{value: 1.0, units: :km}) == %Distance{value: 1.0, units: :km}
+  test "dist - to kilometers - m", do: assert Distance.to_km(%Distance{value: 1000, units: :m}) == %Distance{value: 1.0, units: :km}
+  test "dist - to kilometers - nm", do: assert Distance.to_km(%Distance{value: 1000, units: :nm}) == %Distance{value: 1852.0, units: :km}
+  
+  test "dist - to kilometers - ft" do  
     ftToKm = Distance.to_km(%Distance{value: 2500, units: :ft})
     assert ftToKm.units == :km
     true = assert_in_delta(ftToKm.value, 0.762, @conv_delta)
-    
-    #sm to km
+  end
+
+  test "dist - to kilometers - sm " do  
     smToKm = Distance.to_km(%Distance{value: 100.0, units: :sm})
     assert smToKm.units == :km
     true = assert_in_delta(smToKm.value,160.9344, @conv_delta)
   end
 
-  test "dist - to feet" do
-    assert Distance.to_ft(%Distance{value: 500, units: :ft}) == %Distance{value: 500, units: :ft}
-    assert Distance.to_ft(%Distance{value: 1, units: :sm}) == %Distance{value: 5280, units: :ft}
-    
-    #decimal conversions
+  test "dist - to feet - ft", do: assert Distance.to_ft(%Distance{value: 500, units: :ft}) == %Distance{value: 500, units: :ft}
+  test "dist - to feet - sm", do: assert Distance.to_ft(%Distance{value: 1, units: :sm}) == %Distance{value: 5280, units: :ft}
+  test "dist - to feet - m" do  
     mToFt = 300 |> Distance.new |> Distance.to_ft
     assert mToFt.units == :ft
     true = assert_in_delta(mToFt.value, 984.25197, @conv_delta)
-
+  end
+  test "dist - to feet - km" do
     kmToFt = %Distance{ value: 7.62, units: :km} |> Distance.to_ft
     assert kmToFt.units == :ft
     true = assert_in_delta(kmToFt.value, 25000, @conv_delta)
-
+  end
+  test "dist - to feet - nm" do 
     nmToFt = Distance.new(25, :nm) |> Distance.to_ft
     assert nmToFt.units == :ft
     true = assert_in_delta(nmToFt.value, 151902.88725, @conv_delta)
   end
 
-  test"dist - to miles" do
-    assert Distance.to_sm(%Distance{value: 100.0, units: :sm}) == %Distance{value: 100.0, units: :sm}
-    assert Distance.to_sm(%Distance{value: 5280, units: :ft}) == %Distance{value: 1.0, units: :sm}
-    
+  test "dist - to miles - sm", do: assert Distance.to_sm(%Distance{value: 100.0, units: :sm}) == %Distance{value: 100.0, units: :sm}
+  test "dist - to miles - ft", do: assert Distance.to_sm(%Distance{value: 5280, units: :ft}) == %Distance{value: 1.0, units: :sm}
+  test "dist - to miles - km" do
     kmToSm = Distance.new(25.0, :km) |> Distance.to_sm 
     assert kmToSm.units == :sm
     true = assert_in_delta(kmToSm.value, 15.534279, @conv_delta)
-   
+  end
+  test "dist - to miles - m" do  
     mToSm = 3400.0 |> Distance.new |> Distance.to_sm
     assert mToSm.units == :sm
     true = assert_in_delta(mToSm.value, 2.112662 , @conv_delta)
-
+  end
+  test "dist - to miles - nm" do
     nmToSm = Distance.new(200, :nm) |> Distance.to_sm
     assert nmToSm.units == :sm
     true = assert_in_delta(nmToSm.value, 230.15589, @conv_delta)
   end
 
-  test "dist - to nautical miles" do
-    assert Distance.to_nm(%Distance{value: 152.34, units: :nm}) == %Distance{value: 152.34, units: :nm}
-
-    assert Distance.to_nm(%Distance{value: 1.852, units: :km}) == %Distance{value: 1.0, units: :nm}
-    assert Distance.to_nm(%Distance{value: 3704, units: :m}) == %Distance{value: 2.0, units: :nm}
-
+  test "dist - to nautical miles - nm", do: assert Distance.to_nm(%Distance{value: 152.34, units: :nm}) == %Distance{value: 152.34, units: :nm}
+  test "dist - to nautical miles - km", do: assert Distance.to_nm(%Distance{value: 1.852, units: :km}) == %Distance{value: 1.0, units: :nm}
+  test "dist - to nautical miles - m", do: assert Distance.to_nm(%Distance{value: 3704, units: :m}) == %Distance{value: 2.0, units: :nm}
+  test "dist - to nautical miles - sm" do
     smToNm = 230.15589 |> Distance.new(:sm) |> Distance.to_nm
     assert smToNm.units == :nm
     true = assert_in_delta(smToNm.value, 200, @conv_delta)
-
+  end
+  test "dist - to nautical miles - ft" do
     ftToNm = 3038.057745 |> Distance.new(:ft) |> Distance.to_nautical_miles
     assert ftToNm.units == :nm
     true = assert_in_delta(ftToNm.value, 0.5, @conv_delta)
