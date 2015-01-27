b9344021fafa6db154d79a1756efda2ac0613433
Rodney Norris <Rodney.Norris@gmail.com>
2015-01-26 23:00:40 -0600

    remove geopotion base files, unused currently

--- lib/geopotion.ex
+++
@@ -1,7 +0,0 @@
-defmodule GeoPotion do
-  @moduledoc """
-    A module for interacting with Geo Spatial data
-  """
-  
-   
-end
--- test/geopotion_test.exs
+++
@@ -1,6 +0,0 @@
-defmodule GeoPotionTest do
-  use ExUnit.Case
-  #doctest
-
-end
-
